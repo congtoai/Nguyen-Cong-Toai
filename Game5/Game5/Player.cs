60a5a13d3a51dd8f5fe675caa2fe220aeff5a401
Nguyen Cong Toai <toaitb97@gmail.com>
2018-07-26 11:37:50 +0700

    Game

---
+++ Game5/Game5/Player.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Game5
+{
+    class Player
+    {
+        protected int a, a1, a2, b, b1, b2, x, y;
+        public int GetA()
+        {
+
+            return a;
+        }
+        public int GetA1()
+        {
+
+            return a1;
+        }
+        public int GetA2()
+        {
+
+            return a;
+        }
+        public int GetB()
+        {
+            return b;
+        }
+        public int GetB1()
+        {
+            return b1;
+        }
+        public int GetB2()
+        {
+            return b2;
+        }
+        public int GetX()
+        {
+            return x;
+        }
+        public int GetY()
+        {
+            return y;
+        }
+        public void SetIDNhap(string o)
+        {
+            arr[a, b] = o;
+            arr[a1, b1] = o;
+            arr[a2, b2] = o;
+        }
+       
+        public string GetIDNhap1()
+        {
+            
+            return arr[a, b];
+        }
+        public string GetIDNhap2()
+        {
+            return arr[a1, b1];
+            
+        }
+        public string GetIDNhap3()
+        {
+            
+            return arr[a2, b2];
+        }
+        public string GetIDBan()
+        {
+            return arr[x, y];
+        }
+        public void SetIDBan(string o)
+        {
+            arr[x, y] = o;
+        }
+        public Player()
+        {
+
+        }
+        protected string[,] arr = {
+                            {"-","-","-","-","-"},
+                            {"-","-","-","-","-"},
+                            {"-","-","-","-","-"},
+                            {"-","-","-","-","-"},
+                            {"-","-","-","-","-"}
+                        };
+        int n = 0;
+        public void Xuat()
+        {
+            Console.WriteLine("    0   1   2   3   4");
+            for (int i = 0; i < arr.GetLength(0); i++)
+            {
+                Console.Write(i);
+                for (int j = 0; j < arr.GetLength(1); j++)
+                {
+
+                    Console.Write("{0,4}", arr[i, j]);
+                }
+                Console.WriteLine();
+            }
+        }
+        public void Nhap()
+        {
+
+            Console.WriteLine("Nhap toa do 3 diem cua con thuyen: ");
+            nhaplai:
+            Console.WriteLine("Nhap toa do diem 1 : ");
+
+            a = Convert.ToInt32(Console.ReadLine());
+            b = Convert.ToInt32(Console.ReadLine());
+            if (a < 0 || a > 4 || b < 0 || b > 4)
+            {
+                Console.WriteLine("Nhap sai toa do, nhap lai: ");
+                goto nhaplai;
+            }
+            nhaplai1:
+            Console.WriteLine("Nhap toa do diem 2 : ");
+            a1 = Convert.ToInt32(Console.ReadLine());
+            b1 = Convert.ToInt32(Console.ReadLine());
+            if (a1 < 0 || a1 > 4 || b1 < 0 || b1 > 4)
+            {
+                Console.WriteLine("Nhap sai toa do, nhap lai: ");
+                goto nhaplai1;
+            }
+            nhaplai2:
+            Console.WriteLine("Nhap toa do diem 3 : ");
+            a2 = Convert.ToInt32(Console.ReadLine());
+            b2 = Convert.ToInt32(Console.ReadLine());
+            if (a2 < 0 || a2 > 4 || b2 < 0 || b2 > 4)
+            {
+                Console.WriteLine("Nhap sai toa do, nhap lai: ");
+                goto nhaplai2;
+            }
+        }
+        public void Ban()
+        {
+
+            nhaplai:
+            Console.WriteLine("Nhap toa do diem ban: ");
+            x = Convert.ToInt32(Console.ReadLine());
+            y = Convert.ToInt32(Console.ReadLine());
+
+            if (x < 0 || x > 4 || y < 0 || y > 4)
+            {
+                Console.WriteLine("nhap sai toa do,nhap lai:");
+                goto nhaplai;
+            }
+
+        }
+    }
+}
---
+++ Game5/Game5/Program.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Game5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 0;
+            int n1 = 0;
+            Player player1 = new Player();
+            Player player2 = new Player();
+            player1.Xuat();
+            Console.WriteLine("Nguoi choi 1:");
+            player1.Nhap();
+            player1.SetIDNhap("x");
+            player1.Xuat();
+            Console.WriteLine("Nguoi choi 2:");
+            player2.Nhap();
+            player2.SetIDNhap("+");
+            player2.Xuat();
+            Console.WriteLine("******************* BAT DAU CHOI ****************");
+         banlai:
+            Console.WriteLine("Den luot nguoi choi 1:");
+            player1.Ban();
+            if (player1.GetIDBan() == player1.GetIDNhap1() ||
+                player1.GetIDBan() == player1.GetIDNhap2() ||
+                player1.GetIDBan() == player1.GetIDNhap3())
+            {
+                Console.WriteLine("Khong duoc ban vao thuyen cua minh: ban lai");
+                goto banlai;
+            }
+            else if (player1.GetX() == player2.GetA() && player1.GetY() == player2.GetB() ||
+                   player1.GetX() == player2.GetA1() && player1.GetY() == player2.GetB1() ||
+                   player1.GetX() == player2.GetA2() && player1.GetY() == player2.GetB2())
+            {
+                Console.WriteLine("Chuc Mung: ban trung thuyen dich: ");
+                player1.SetIDBan("T");
+                player1.Xuat();
+                Console.WriteLine("--------------------------------------------------------");
+                
+                n++;
+                if (n == 3)
+                {
+                    {
+                        Console.WriteLine("You Win: Game Over");
+                        Console.WriteLine("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+                        goto gameover;
+                    }
+                }
+            }
+         
+            else if(player1.GetIDBan() == "T")
+            {
+                Console.WriteLine("Diem nay da duoc ban roi: ban lai ");
+                goto banlai;
+            }
+            else
+            {
+                Console.WriteLine("Huhu: Ban xit roi:");
+                player1.SetIDBan("o");
+                player1.Xuat();
+                Console.WriteLine("--------------------------------------------------------");
+                
+            }
+
+            banlai1:
+            Console.WriteLine("den luot nguoi choi 2:");
+            player2.Ban();
+            if (player2.GetIDBan() == "+")
+            {
+                Console.WriteLine("Khong duoc ban vao thuyen cua minh: ban lai");
+                goto banlai1;
+            }
+            else if (player2.GetX() == player1.GetA() && player2.GetY() == player1.GetB() ||
+                   player2.GetX() == player1.GetA1() && player2.GetY() == player1.GetB1() ||
+                   player2.GetX() == player1.GetA2() && player2.GetY() == player1.GetB2())
+            {
+                Console.WriteLine("Chuc Mung: ban trung thuyen dich: ");
+                player2.SetIDBan("T");
+                player2.Xuat();
+                Console.WriteLine("--------------------------------------------------------");
+                
+                n1++;
+                if (n1 == 3)
+                {
+                    {
+                        Console.WriteLine("You Win: Game Over");
+                        Console.WriteLine("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+                        goto gameover;
+                    }
+                }
+            }
+            else if (player2.GetIDBan() == "T")
+            {
+                Console.WriteLine("Diem nay da duoc ban roi: ban lai ");
+                goto banlai1;
+            }
+            else
+            {
+                Console.WriteLine("Huhu: Ban xit roi:");
+                player2.SetIDBan("o");
+                player2.Xuat();
+                Console.WriteLine("--------------------------------------------------------");
+                
+            }
+            goto banlai;
+            gameover:
+            Console.ReadKey();
+
+        }
+    }
+
+}
